2496d4183df764085211d28fe2db0887e1640ea6
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2021-07-09 19:23:08 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,50 @@
+package com.brainmentors.dsa.searching;
+
+public class BinarySearch {
+	
+	public static int binarySearch(int arr[], int first, int last, int search)
+	{
+		int mid = (first + last) / 2;
+		
+		while(first <= last)
+		{
+			if(arr[mid] == search)
+			{
+				return mid;
+			}
+			else if(arr[mid] < search)
+			{
+				first = mid + 1;
+			}
+			else if(arr[mid] > search)
+			{
+				last = mid - 1;
+			}
+			
+			mid = (first + last)/2;
+		}
+		
+		return -1;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[] = {2, 3, 4, 9, 10, 20, 35, 90};
+		int n = arr.length;
+		
+		int search = 35;
+		
+		int result = binarySearch(arr, 0, n - 1, search);
+		
+		if(result == -1)
+		{
+			System.out.println("Element not Found");
+		}
+		else {
+			System.out.println("Element Found");
+		}
+	}
+
+}
